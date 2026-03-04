c26fac7b1f150c7c8bf15507b9f53f3084b42b32
David <david125794@gmail.com>
2026-03-04 14:59:58 +0100

    Sprites push

---
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    [Header("Visual Cue")]
+    [SerializeField] private GameObject visualCue;
+    
+    [Header("Ink JSON")]
+    
+    private bool playerInRange;
+    
+    private void Awake()
+    {
+        playerInRange = false;
+        visualCue.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (playerInRange)
+        {
+            visualCue.SetActive(true);
+        }
+        else
+        {
+            visualCue.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
