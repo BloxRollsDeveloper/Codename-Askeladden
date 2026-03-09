4086b2011958183bf7a33366649ee51f82edd640
Marmine15 <martinandreas2006@gmail.com>
2026-03-09 14:32:23 +0100

    attacking is set

--- Assets/Scripts/Input/PlayerAttack.cs
+++ Assets/Scripts/Input/PlayerAttack.cs
@@ -2,13 +2,39 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    public Animator anim;
+    [SerializeField] private Animator anim;
+    [SerializeField] private Transform weapon;
+    [SerializeField] private float attackRange;
+    [SerializeField] private LayerMask whatIsEnemy;
+    
 
     public void UpdateAttack(bool isAttacking)
     {
         if (isAttacking)
         {
-            print("Playing the attack animation!");
+            anim.Play("SwordAttack");
+            CheckForEnemies();
         }
+        else
+        {
+            anim.Play("SwordHover");
+        }
+    }
+
+    private void CheckForEnemies()
+    {
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(weapon.position, attackRange, whatIsEnemy);
+
+        if (enemies.Length > 0)
+        {
+            print("enemies found");
+            // TODO: get the enemy health script from here and affect them.
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(weapon.position, attackRange);
     }
 }
--- Assets/Scripts/Input/PlayerController.cs
+++ Assets/Scripts/Input/PlayerController.cs
@@ -1,19 +1,22 @@
 using UnityEngine;
 
-[RequireComponent(typeof(PlayerInput), typeof(PlayerMove))]
+[RequireComponent(typeof(PlayerInput), typeof(PlayerMove), typeof(PlayerAttack))]
 public class PlayerController : MonoBehaviour
 {
     private PlayerInput _playerInput;
     private PlayerMove _playerMove;
+    private PlayerAttack _playerAttack;
 
     private void Start()
     {
         _playerInput = GetComponent<PlayerInput>();
         _playerMove = GetComponent<PlayerMove>();
+        _playerAttack = GetComponent<PlayerAttack>();
     }
 
     private void Update()
     {
         _playerMove.UpdateMovement(_playerInput.Movement, false);
+        _playerAttack.UpdateAttack(_playerInput.Attack);
     }
 }
