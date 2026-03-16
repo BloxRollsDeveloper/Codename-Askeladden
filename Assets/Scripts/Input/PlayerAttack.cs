b43009d1983e7f91023c916cde2778bd9c9992c8
Marmine15 <martinandreas2006@gmail.com>
2026-03-16 14:26:06 +0100

    currently making a new

--- Assets/Scripts/Input/PlayerAttack.cs
+++ Assets/Scripts/Input/PlayerAttack.cs
@@ -17,12 +17,15 @@ public class PlayerAttack : MonoBehaviour
     [SerializeField] private float attackRange;
     [SerializeField] private LayerMask whatIsEnemy;
     [SerializeField] private float damage;
+    [SerializeField] private float attackAnimTime;
     
     private PlayerController _playerController;
+    private PlayerDirection _playerDirection;
 
     private void Start()
     {
         _playerController = GetComponent<PlayerController>();
+        _playerDirection = GetComponent<PlayerDirection>();
     }
     
     public void UpdateAttack(bool isAttacking)
@@ -35,7 +38,8 @@ public class PlayerAttack : MonoBehaviour
 
     private void CheckForEnemies()
     {
-        _playerController.animationState = PlayerController.AnimationState.Attack;
+        // _playerController.animationState = PlayerController.AnimationState.Attack;
+        UpdateAttackDirection(_playerDirection);
         
         Collider2D[] enemies = Physics2D.OverlapCircleAll(activeAttackPoint.position, attackRange, whatIsEnemy);
 
@@ -55,38 +59,48 @@ public class PlayerAttack : MonoBehaviour
         switch (direction.direction)
         {
             case PlayerDirection.Direction.North:
-                anim.Play("AttackNorth");
+                anim.CrossFade(North, attackAnimTime);
                 activeAttackPoint = attackPoint[0];
                 break;
             case PlayerDirection.Direction.East:
-                anim.Play("AttackEast");
+                anim.CrossFade(East, attackAnimTime);
                 activeAttackPoint = attackPoint[1];
                 break;
             case PlayerDirection.Direction.South:
-                anim.Play("AttackSouth");
+                anim.CrossFade(South, attackAnimTime);
                 activeAttackPoint = attackPoint[2];
                 break;
             case PlayerDirection.Direction.West:
-                anim.Play("AttackWest");
+                anim.CrossFade(West, attackAnimTime);
                 activeAttackPoint = attackPoint[3];
                 break;
             case PlayerDirection.Direction.NorthEast:
-                anim.Play("AttackNorthEast");
+                anim.CrossFade(NorthEast, attackAnimTime);
                 activeAttackPoint = attackPoint[4];
                 break;
             case PlayerDirection.Direction.NorthWest:
-                anim.Play("AttackNorthWest");
+                anim.CrossFade(NorthWest, attackAnimTime);
                 activeAttackPoint = attackPoint[5];
                 break;
             case PlayerDirection.Direction.SouthEast:
-                anim.Play("AttackSouthEast");
+                anim.CrossFade(SouthEast, attackAnimTime);
                 activeAttackPoint = attackPoint[6];
                 break;
             case PlayerDirection.Direction.SouthWest:
-                anim.Play("AttackSouthWest");
+                anim.CrossFade(SouthWest, attackAnimTime);
                 activeAttackPoint = attackPoint[7];
                 break;
         }
     }
+    
+    // Attack Animations
+    private static readonly int North = Animator.StringToHash("AttackNorth");
+    private static readonly int East = Animator.StringToHash("AttackEast");
+    private static readonly int South = Animator.StringToHash("AttackSouth");
+    private static readonly int West = Animator.StringToHash("AttackWest");
+    private static readonly int NorthEast = Animator.StringToHash("AttackNorthEast");
+    private static readonly int NorthWest = Animator.StringToHash("AttackNorthWest");
+    private static readonly int SouthEast = Animator.StringToHash("AttackSouthEast");
+    private static readonly int SouthWest = Animator.StringToHash("AttackSouthWest");
     
 }
