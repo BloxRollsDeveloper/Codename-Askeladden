e154e2ef7c177eabe525c8ebfe1af8a5af64771f
Marmine15 <martinandreas2006@gmail.com>
2026-03-21 12:50:35 +0100

    huldra is finished

--- Assets/Scripts/Enemys/Huldra/NewHuldraFR.cs
+++ Assets/Scripts/Enemys/Huldra/NewHuldraFR.cs
@@ -30,22 +30,27 @@ public class NewHuldraFR : MonoBehaviour
     private HuldraAnimationController _animationController;
     private Vector2 _moveDirection;
 
-    [Header("Target")] 
-    private Transform _target;
-    private Vector3 _targetPoint;
-    [SerializeField] private float targetDistance;
-    [SerializeField] private float attackRange;
-    [SerializeField] private float runRange;
-    [SerializeField] private float targetMargin; // how close huldra needs to be to a point to stop.
+    private float xDirection;
+    private float yDirection;
 
-    public int changeRarity = 25;
+    [Header("Targeting")] 
+    private Transform _target;
+    [SerializeField] private float attackRange;
+    [SerializeField] private float runAwayRange;
+    [SerializeField] private float approachRange;
+    
+    [Header("Attack")]
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private float projectileSpeed;
+    [SerializeField] private float projectileLifetime;
+    [SerializeField] private float attackDelay = 5f;
+    [SerializeField] private float timer;
     
     private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _animationController = GetComponent<HuldraAnimationController>();
         
-        _targetPoint = transform.position;
         _target = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
@@ -53,8 +58,62 @@ public class NewHuldraFR : MonoBehaviour
     {
         if (_target == null) return;
         
+        xDirection = _target.position.x -  transform.position.x;
+        yDirection = _target.position.y -  transform.position.y;
+        var distanceToPlayer = Mathf.Sqrt(Mathf.Abs(xDirection * xDirection) + Mathf.Abs(yDirection * yDirection));
+
+        if (xDirection < 0) xDirection = -1f;
+        else if (xDirection > 0) xDirection = 1f;
+        
+        if (yDirection < 0) yDirection = -1f;
+        else if (yDirection > 0) yDirection = 1f;
+
+        if (!(distanceToPlayer > runAwayRange))
+        {
+            state = State.Walk;
+            _moveDirection = (_target.position - transform.position).normalized;
+            _animationController.UpdateMoveDirection(-1 * _moveDirection);
+            SwitchAnimation(Animation.Walk, false,false, false);
+            
+            _rigidbody2D.linearVelocityX = -1 * xDirection * moveSpeed;
+            _rigidbody2D.linearVelocityY = -1 * yDirection * moveSpeed;
+        }
+        else if (distanceToPlayer > approachRange)
+        {
+            state = State.Walk;
+            _moveDirection = (_target.position - transform.position).normalized;
+            _animationController.UpdateMoveDirection(_moveDirection);
+            SwitchAnimation(Animation.Walk, false,false, false);
+            
+            _rigidbody2D.linearVelocityX = xDirection * moveSpeed;
+            _rigidbody2D.linearVelocityY = yDirection * moveSpeed;
+        }
+        else
+        {
+            print("Huldra should attack.");
+            _rigidbody2D.linearVelocity = Vector2.zero;
+            state = State.Idle;
+            _animationController.UpdateMoveDirection(Vector2.zero);
+            SwitchAnimation(Animation.Idle, false, false, false);
+            
+            timer += Time.deltaTime;
+
+            if (timer >= attackDelay)
+            {
+                state = State.Attack;
+                _animationController.UpdateMoveDirection(_target.position - transform.position); 
+                SwitchAnimation(Animation.Attack, true, false, false);
+            }
+        }
+        
+        if (true) return;
+        
+        /* TODO: Remove this
+         
+        if (_target == null) return;
+        
         var dist = Vector2.Distance(_target.position, transform.position);
-        if (dist <= runRange)
+        if (dist <= runAwayRange)
         {
             ResetTargetPoint();
             Walk();
@@ -75,6 +134,7 @@ public class NewHuldraFR : MonoBehaviour
         }
         else
         {
+            print("I am stopping here!");
             state = State.Idle;
             _rigidbody2D.linearVelocity = Vector2.zero;
             _animationController.UpdateMoveDirection(Vector2.zero);
@@ -83,6 +143,7 @@ public class NewHuldraFR : MonoBehaviour
 
         if (Random.Range(1, changeRarity) > 1) return;
         ResetTargetPoint();
+        */
     }
 
     private void SwitchAnimation(Animation anim, bool isAttacking, bool isDamaged, bool isDead)
@@ -94,28 +155,24 @@ public class NewHuldraFR : MonoBehaviour
         }
     }
 
-    private void ResetTargetPoint()
+    public void AttackPlayer()
     {
-        _targetPoint = Vector3.Normalize(_target.position - transform.position) * (Vector2.Distance(_target.position, transform.position) - targetDistance);
-    }
+        var playerDirection = _target.position - transform.position;
+        var projectileClone = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        
+        Destroy(projectileClone, projectileLifetime);
+        
+        projectileClone.TryGetComponent(out Rigidbody2D projectileRb);
+        projectileRb.linearVelocity = playerDirection.normalized * projectileSpeed;
 
-    private void Walk()
-    {
-        state = State.Walk;
-        _moveDirection = (_targetPoint - transform.position).normalized;
-        _animationController.UpdateMoveDirection(_moveDirection);
-        SwitchAnimation(Animation.Walk, false,false, false);
-            
-        _rigidbody2D.linearVelocity = _moveDirection * moveSpeed;
+        timer = 0f;
     }
 
     private void OnDrawGizmosSelected()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, attackRange);
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, targetDistance);
+        Gizmos.DrawWireSphere(transform.position, approachRange);
         Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position, runRange);
+        Gizmos.DrawWireSphere(transform.position, runAwayRange);
     }
 }
