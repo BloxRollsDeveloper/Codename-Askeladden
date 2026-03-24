6275da1251dc5fb7baa2bc0a459cd2d39f772d9f
David <david125794@gmail.com>
2026-03-24 21:55:16 +0100

    Pull

---
+++ Assets/Scripts/Enemys/SpawnManager/BabyTrollSpawnManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class BabyTrollSpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject babyTrollPrefab;
+    [SerializeField] private Transform[] babyTrollSpawnPoints;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
